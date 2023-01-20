676f742325b547cf5393cedbb394650b77451ca2
DrTulipe <contact.me@kilian-naudin.com>
2023-01-20 13:39:05 +0100

    fix

--- src/TestPage.tsx
+++ src/TestPage.tsx
@@ -5,7 +5,7 @@ import { Navbar } from "./Framework/Navbar/NavBar";
 export function TestPage() {
   return (
     <>
-      <Navbar />
+      <Navbar childrens={""}/>
       <h1 className="text-3xl font-bold underline text-red-500">
         Hello world aaa!
       </h1>
