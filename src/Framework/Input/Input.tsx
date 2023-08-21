b5b78f6691b84e6f84ded85999aecdaa1a61ddcd
Kilian Naudin <kilian.naudin22@gmail.com>
2023-08-21 15:49:27 +0200

    wip : login

--- src/Framework/Input/Input.tsx
+++ src/Framework/Input/Input.tsx
@@ -1,40 +1,43 @@
 import classNames from 'classnames';
-import React, { useId } from 'react';
+import React, { forwardRef, useId } from 'react';
 import { FaAsterisk } from 'react-icons/fa';
 
 type Props = {
-  name: string,
-  label?: string,
-  type?: 'text' | 'password' | 'email'
-  defaultValue?: any
-  required?: boolean
-  placeholder?: string
+  name: string;
+  label?: string;
+  type?: 'text' | 'password' | 'email';
+  defaultValue?: any;
+  required?: boolean;
+  placeholder?: string;
 };
 
-export const Input: React.FC<Props> = ({name, label, type, defaultValue, required, placeholder }) => {
+export const Input: React.ForwardRefRenderFunction<HTMLInputElement, Props> = (
+  { name, label, type='text', defaultValue, required, placeholder },
+  ref
+) => {
   const inputId = useId();
 
   const labelClasses = classNames("text-semibold text-gray-700 flex gap-2 items-center");
   const inputClasses = classNames("w-full border rounded p-2 invalid:border-red-500");
 
-  return (<>
-  <div className="flex flex-col gap-2">
-    {label && <label className={labelClasses} htmlFor={inputId}>
-      {/* <div>{label}</div> */}
-       {required && (<div><FaAsterisk className="text-red-500 text-sm" /></div>)}
-    </label>}
-    <input
-      required={required}
-      className={inputClasses}
-      name={name}
-      type={type}
-      defaultValue={defaultValue}
-      placeholder={placeholder || label || undefined}
-    />
-  </div>
-  </>);
-}
-
-Input.defaultProps = {
-  type: 'text'
+  return (
+    <div className="flex flex-col gap-2">
+      {label && (
+        <label className={labelClasses} htmlFor={inputId}>
+          {required && <FaAsterisk className="text-red-500 text-sm" />}
+        </label>
+      )}
+      <input
+        required={required}
+        className={inputClasses}
+        name={name}
+        type={type}
+        ref={ref}
+        defaultValue={defaultValue}
+        placeholder={placeholder || label || undefined}
+      />
+    </div>
+  );
 };
+
+export const InputForwarded = forwardRef(Input);
--- src/Framework/Navbar/NavBar.tsx
+++ src/Framework/Navbar/NavBar.tsx
@@ -3,6 +3,7 @@ import { FaChevronDown } from "react-icons/fa";
 import { GiChickenOven } from "react-icons/gi";
 import BrandIconUrl from "../../../public/images-logo/PLANNING BY DAY Logo - Original.svg";
 import { useNavigate } from "react-router-dom";
+import { isLogged } from "../../Login/LoginPage";
 
 export function Navbar() {
   const navigate = useNavigate();
@@ -21,66 +22,75 @@ export function Navbar() {
         </a>
       </div>
       <div className="flex-none">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a
-              onClick={() => {
-                navigate("/planning");
-              }}
-            >
-              Planning
-            </a>
-          </li>
-          <li>
-            <a
-              onClick={() => {
-                navigate("/test");
-              }}
-            >
-              Test page
-            </a>
-          </li>
-          <li tabIndex={0}>
-            <a>
-              <div className="avatar">
-                <div className="w-10 rounded-full">
-                  <img src="https://placeimg.com/192/192/people" />
+        {isLogged ?
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <a
+                onClick={() => {
+                  navigate("/planning");
+                }}
+              >
+                Planning
+              </a>
+            </li>
+            <li>
+              <a
+                onClick={() => {
+                  navigate("/test");
+                }}
+              >
+                Test page
+              </a>
+            </li>
+            <li tabIndex={0}>
+              <a>
+                <div className="avatar">
+                  <div className="w-10 rounded-full">
+                    <img src="https://placeimg.com/192/192/people" />
+                  </div>
                 </div>
-              </div>
-              Espace personnel
-              <FaChevronDown />
+                Espace personnel
+                <FaChevronDown />
+              </a>
+              <ul className="p-2 bg-base-100">
+                <li>
+                  <a
+                    onClick={() => {
+                      navigate("/account");
+                    }}
+                  >
+                    Mon compte
+                  </a>
+                </li>
+                <li>
+                  <a
+                    onClick={() => {
+                      navigate("/formations");
+                    }}
+                  >
+                    Mes formations
+                  </a>
+                </li>
+                <li>
+                  <a
+                    onClick={() => {
+                      navigate("/logout");
+                    }}
+                  >
+                    Me déconnecter
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+          : <div className="flex-1">
+            <a
+              href="/login"
+              className="btn btn-ghost normal-case text-xl overflow-hidden"
+            >
+              Se connecter
             </a>
-            <ul className="p-2 bg-base-100">
-              <li>
-                <a
-                  onClick={() => {
-                    navigate("/account");
-                  }}
-                >
-                  Mon compte
-                </a>
-              </li>
-              <li>
-                <a
-                  onClick={() => {
-                    navigate("/formations");
-                  }}
-                >
-                  Mes formations
-                </a>
-              </li>
-              <li>
-                <a
-                  onClick={() => {
-                    navigate("/logout");
-                  }}
-                >
-                  Me déconnecter
-                </a>
-              </li>
-            </ul>
-          </li>
-        </ul>
+          </div>}
       </div>
     </div>
   );
