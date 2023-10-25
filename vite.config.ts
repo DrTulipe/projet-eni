5c7cfbb43794b6ce99bff682cde47de5ee47f181
Kilian Naudin <kilian.naudin22@gmail.com>
2023-10-25 11:25:46 +0200

    url en dur

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://optiplanning.ods-dual.fr/',
-        changeOrigin: true,
-        secure: false,
-        // rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
 });
