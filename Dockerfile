71c7d68e275f2a38d7db11b62ffb23416c1d5c63
Kilian Naudin <knaudin@synapsy.fr>
2023-10-30 15:15:33 +0100

    ok

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM nginx
 COPY ./dist/ /usr/share/nginx/html
-COPY ./front.conf /etc/nginx/sites-available/default
+COPY ./front.conf /etc/nginx/conf.d/default.conf
 EXPOSE 80
--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8000/',
-        changeOrigin: true,
-        secure: false,
-        // rewrite: (path) => path.replace(/^/api/, '')
-      }
-    }
-  }
 });
