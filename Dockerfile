e1da0a9583f2b2fe0006d8c8aee10b59726ce7a4
Yann Van Dyck <yvandyck@synapsy.fr>
2023-10-30 17:32:26 +0100

    HTTPS added

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM nginx
 COPY ./dist/ /usr/share/nginx/html
 COPY ./front.conf /etc/nginx/conf.d/default.conf
-EXPOSE 80
+EXPOSE 80 443
--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://projet-eni-back:8000/',
+        target: 'https://projet-eni-back:8000/',
         changeOrigin: true,
         secure: false,
         // rewrite: (path) => path.replace(/^/api/, '')
