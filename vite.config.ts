1ed8ed5359d7ae35bce85079b6e4472d8ae60027
Yann Van Dyck <yvandyck@synapsy.fr>
2023-10-30 16:19:14 +0100

    correction vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://projet-eni-back:8000/',
+        changeOrigin: true,
+        secure: false,
+        // rewrite: (path) => path.replace(/^/api/, '')
+      }
+    }
+  }
 });
